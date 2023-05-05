15990b6e66c861beea686b43d310f5498f0c4d5b
beefybroccoli <tedkimemail@yahoo.com>
2023-05-04 21:54:32 -0700

    ...

--- enrollment/env_enrollment/main.py
+++ enrollment/env_enrollment/main.py
@@ -1,13 +1,15 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
-@app.route("/contact")
+# @app.route("/")
+# @app.route("/home")
+# @app.route("/contact")
 
-def home():
-    return "<h1>Hello world - home page with more ...<h1>"
+# def home():
+#     return "<h1>Hello world - home page with more ...<h1>"
 
-def contact():
-    return "<h1>Hello world - contact page<h1>"
+# def contact():
+#     return "<h1>Hello world - contact page<h1>"
+
+from application import app
