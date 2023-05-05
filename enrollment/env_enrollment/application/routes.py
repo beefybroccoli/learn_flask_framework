21f9d7d1381b9c1ca4b93127424ff4410f57e1df
beefybroccoli <tedkimemail@yahoo.com>
2023-05-05 16:56:40 -0700

    ...

--- enrollment/env_enrollment/application/routes.py
+++ enrollment/env_enrollment/application/routes.py
@@ -2,12 +2,24 @@ from application import app
 from flask import render_template
 
 @app.route("/")
+@app.route("/index")
 @app.route("/home")
-@app.route("/contact")
-
 def home():
     # return "<h1>Hello world - home page with more ...<h1>"
     return render_template("index.html", login=True)
 
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
