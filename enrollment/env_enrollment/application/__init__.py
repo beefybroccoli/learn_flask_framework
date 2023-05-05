8186b0d3dd7de4a9e1355be5763df50709ff6732
beefybroccoli <tedkimemail@yahoo.com>
2023-05-04 21:51:08 -0700

    ...

---
+++ enrollment/env_enrollment/application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+@app.route("/contact")
+
+def home():
+    return "<h1>Hello world - home page with more ...<h1>"
+
+def contact():
+    return "<h1>Hello world - contact page<h1>"
