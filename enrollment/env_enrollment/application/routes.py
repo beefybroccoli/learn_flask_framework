2b8ebfa1d8d1a2050e8bd32b9040ea1169f6f795
beefybroccoli <tedkimemail@yahoo.com>
2023-05-05 17:49:38 -0700

    ...

--- enrollment/env_enrollment/application/routes.py
+++ enrollment/env_enrollment/application/routes.py
@@ -6,20 +6,22 @@ from flask import render_template
 @app.route("/home")
 def home():
     # return "<h1>Hello world - home page with more ...<h1>"
-    return render_template("index.html", login=True)
+    return render_template("index.html", home=True)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", contact=True)
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html")
+    courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}];
+    print(courseData)
+    return render_template("courses.html", courseData=courseData,courses=True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("register.html", register=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", login=True)
